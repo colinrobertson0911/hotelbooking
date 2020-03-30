b9d11e5a1dcad539f8adb5d05b42dd49059ced04
gwise <gwise@LAPTOP-TQC5MS5T>
2020-03-30 14:52:30 +0100

    Started off front end:
    Created login controller for hotel owner, hotelownerhome.jsp and
    loginowner.jsp

--- src/main/java/com/fdmgroup/hotelbookingsystem/controller/HotelOwnerController.java
+++ src/main/java/com/fdmgroup/hotelbookingsystem/controller/HotelOwnerController.java
@@ -1,5 +1,53 @@
 package com.fdmgroup.hotelbookingsystem.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fdmgroup.hotelbookingsystem.model.HotelOwner;
+import com.fdmgroup.hotelbookingsystem.services.HotelOwnerService;
+
+@Controller
 public class HotelOwnerController {
 
-}
+public final static String SESSION_ATTRIBUTE_HOTELOWNER = "HOTELOWNER";
+	
+	@Autowired
+	HotelOwnerService hotelOwnerService;
+	
+	@GetMapping("/")
+	public String main() {
+		return "MainScreen.jsp";
+	}
+	
+	@GetMapping("LoginAsOwner")
+	public String login() {
+		return "loginOwner.jsp";
+	}
+	
+	@PostMapping("LoginSubmit")
+	public ModelAndView loginSubmit(@ModelAttribute("HotelOwner")HotelOwner hotelOwner, 
+			ModelMap model, HttpSession session) { 
+		HotelOwner hotelownerfromdatabase = hotelOwnerService.findByUsernameAndPassword(hotelOwner.getUsername(),
+				hotelOwner.getPassword());
+		
+		if (hotelownerfromdatabase == null) {
+			model.addAttribute("errorMessage", "Incorrect username or password");
+			return new ModelAndView("loginOwner.jsp");
+		}
+		
+		session.setAttribute(SESSION_ATTRIBUTE_HOTELOWNER, hotelownerfromdatabase);
+		return new ModelAndView("HotelOwnerHome.jsp");
+		
+	}
+	
+	
+	}
+	
+
