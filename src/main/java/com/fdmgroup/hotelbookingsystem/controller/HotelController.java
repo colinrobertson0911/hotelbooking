041134ad0962200f0eaf1bf50f61668f200a9608
david.currie <david.currie@fdmgroup.com>
2020-03-30 16:45:48 +0100

    MainScreen shows hotels, updated Hotelcontroller

--- src/main/java/com/fdmgroup/hotelbookingsystem/controller/HotelController.java
+++ src/main/java/com/fdmgroup/hotelbookingsystem/controller/HotelController.java
@@ -1,7 +1,10 @@
 package com.fdmgroup.hotelbookingsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,19 +18,30 @@ public class HotelController {
 
 	@Autowired
 	HotelService hotelService;
-	
-	@GetMapping("/")
-	public String main() {
-		return "MainScreen.jsp";
-	}
-	
-	@GetMapping("LoginAsOwner")
-	public String login() {
-		return "loginOwner.jsp";
-	}
+
 	@GetMapping("")
 	public ModelAndView home() {
 		return new ModelAndView("MainScreen.jsp", "hotel", hotelService.findAll());
 	}
-	
+
+	@GetMapping("/Home")
+	public ModelAndView homeScreen() {
+		return new ModelAndView("MainScreen.jsp", "hotel", hotelService.findAll());
+	}
+
+	@GetMapping("LoginAsOwner")
+	public String login() {
+		return "loginOwner.jsp";
+	}
+
+	@PostMapping("SearchByCity")
+	public ModelAndView searchByCity(@ModelAttribute("hotel") Hotel hotel, ModelMap model) {
+		List<Hotel> hotelList = hotelService.findByCity(hotel.getCity());
+		if (hotelList.isEmpty()) {
+			model.addAttribute("errorMessage", "Not Hotels in that city");
+			return new ModelAndView("MainScreen.jsp", "hotel", hotelService.findAll());
+		}
+		return new ModelAndView("MainScreen.jsp", "hotel", hotelList);
+	}
+
 }
--- src/main/webapp/MainScreen.jsp
+++ src/main/webapp/MainScreen.jsp
@@ -1,5 +1,7 @@
+
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,15 +9,33 @@
 <title>Main</title>
 </head>
 <body>
-<h2>Main Page</h2>
+	<h2>Main Page</h2>
 
 	<div>
 		<a href="LoginAsOwner">Log In as a Hotel Owner</a>
 	</div>
 	
+	<form action="SearchByCity" method="post">
+	${errorMessage}
+	<div>
+	<label>Search by city:</label>
+	<input type="city" name="city"/>
+	</div>
+	<button type="submit">Search</button>
+	</form>
 	
-	
-	
+
+	<c:forEach items="${hotel}" var="hotel">
+		<h3>${hotel.hotelName}</h3>
+		<div>
+			<p>Number of rooms: ${hotel.numOfRooms}</p>
+			<p>City: ${hotel.city}</p>
+			<p>Star Rating: ${hotel.starRating}/5</p>
+		</div>
+		<div>
+			<p>-------------------------------------------</p>
+		</div>
+	</c:forEach>
 
 </body>
 </html>
