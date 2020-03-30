beaacb0980ff0b96d796c786d05c5c3e1cf0c174
Euan2 <Euan2@LAPTOP-3B8292P2.home>
2020-03-30 11:10:24 +0100

    Created Test Classes for Booking, Hotel and Hotel Owner.

---
+++ src/test/java/com/fdmgroup/hotelbookingsystem/HotelOwnerTest.java
@@ -0,0 +1,14 @@
+package com.fdmgroup.hotelbookingsystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HotelOwnerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
