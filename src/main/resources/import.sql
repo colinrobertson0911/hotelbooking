b33b1aac1744e259c7bf618136d26a0b53ea7f22
david.currie <david.currie@fdmgroup.com>
2020-03-30 14:02:46 +0100

    Hotels have been added to import file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,12 @@
 insert into hotelowner(hotelOwnerId, username, password, email, name) values (HOTELOWNER_SEQ.nextval,'user1','password','user1@email.com','user one');
+
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Travelodge Glasgow', 53, 'Glasgow', 3);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Yotel', 50, 'Edinburgh', 4);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Radisson Blue', 43, 'Glasgow', 4);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Mar Hall' , 112, 'Glasgow', 5);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Travelodge Aberdeen', 90, 'Aberdeen', 3 );
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Friars Wynd', 10, 'Stirling', 2);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Balmoral', 5, 'Edinburgh', 5);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Beehive BnB', 3, 'Dundee', 1);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Sherbrook Hotel', 15, 'Glasgow', 4);
+insert into hotel(hotelId, hotelName, numOfRooms, city, starRating) values (HOTEL_SEQ.nextval, 'Stirling Highland hotel', 25, 'Stirling', 4);
